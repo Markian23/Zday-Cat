cc155a77582104ba8a56237154a3437e95f86f57
Markian <122402937+Markian23@users.noreply.github.com>
2023-01-10 22:45:23 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+$(function() {
+
+    /* Smooth scroll */
+    var $page = $('html, body');
+    $('a[href*="#"]').click(function() {
+        $page.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 400);
+        return false;
+    });
+
+    /* Menu nav toggle */
+    $("#nav_toggle").on("click", function(event) {
+        event.preventDefault();
+
+        $(this).toggleClass("active");
+        $("#nav").toggleClass("active");
+    });
+
+});
